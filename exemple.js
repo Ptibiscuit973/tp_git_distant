c85fd9fe35f3db8d771581a9cb935fccaaa99505
Lulu <regis.ludovic91@gmail.com>
2019-12-16 13:47:51 +0100

    ajout js bold

---
+++ exemple.js
@@ -0,0 +1,10 @@
+function changer(){
+    document.querySelector('button').onclick = function(){
+        document.querySelector('p').style.fontWeight = 'bold';
+    }
+
+}
+
+window.onload = function(){
+    this.changer();
+}
